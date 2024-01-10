6c6dddd0e52aaee0c254db28a608c917a39b8f0c
Tim <tima@nopublic.ru>
2024-01-10 04:12:24 +0300

    Rename to TgKit

--- src/rest/RestManager.ts
+++ src/rest/RestManager.ts
@@ -1,5 +1,5 @@
 import Client from '../client/Client'
-import TelegramBotAPIError from '../errors/TeleScriptError'
+import TelegramBotAPIError from '../errors/TgKitError'
 
 export default class RestManager {
 	client: Client
