fa98edcea03287dd3a4d1e9bf6d6c6cfc881f4c1
Tim Nikolsky <tim@timnik.me>
2025-01-29 16:59:01 +0300

    delete old site

--- apps/site/src/routes/reference/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-import docsModel from '../../../../../.temp/api-ref.json';
-
-export const prerender = true;
-
-export const load: LayoutServerLoad = async () => {
-	return docsModel;
-};
